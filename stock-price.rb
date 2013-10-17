f540d8565b7664389d20580aedc5e23c400367c0
Harry Moreno <morenoh149@gmail.com>
2013-10-17 11:24:04 -0700

    implemented max stock price algorithm in O(n) time

--- stock-price.rb
+++ stock-price.rb
@@ -5,8 +5,16 @@
 
 num_of_mins_in_day = 60*24
 stockPricesYesterday = Array.new(num_of_mins_in_day)
-stockPricesYesterday.map! { |p| rand(1000) }
+stockPricesYesterday.map! { |p| rand(100000) }
 
 def find_best_deal(stockPrices)
-
+  minPrice = stockPrices[0]
+  maxProfit = 0
+  stockPrices.each do |p|
+    minPrice = [minPrice, p].min
+    maxProfit = [maxProfit, p - minPrice].max
+  end
+  maxProfit
 end
+
+puts find_best_deal(stockPricesYesterday)
